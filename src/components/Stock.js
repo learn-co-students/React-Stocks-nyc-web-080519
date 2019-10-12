599cb8b407a62be7f2be8c5ff5b09ded2204d8fe
danielahn <93danielahn@gmail.com>
2019-10-12 15:09:43 -0400

    Done.

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,21 +1,21 @@
 import React from 'react'
 
-const Stock = () => (
-  <div>
-
-    <div className="card">
-      <div className="card-body">
-        <h5 className="card-title">{
-            //Company Name
-          }</h5>
-        <p className="card-text">{
-            //ticker: stock price
-          }</p>
+const Stock = (props) => {
+  let current = props.stock
+  return (
+    <div>
+      <div className="card" onClick={(e) => props.buyStock ? props.buyStock(current) : props.removeStock(current)}>
+        <div className="card-body">
+          <h5 className="card-title">{
+              props.stock.name
+            }</h5>
+          <p className="card-text">{props.stock.ticker}: ${
+              props.stock.price
+            }</p>
+        </div>
       </div>
     </div>
-
-
-  </div>
-);
+  )
+};
 
 export default Stock
--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import Stock from '../components/Stock'
 
-class PortfolioContainer extends Component {
 
+// allow a user to buy a stock by clicking on it and when it is bought, it should be added to My Portfolio.
+
+class PortfolioContainer extends Component {
   render() {
+    let userStocksArr = (this.props.stocks.length > 0) ? (this.props.stocks.map(stock => <Stock key={stock.id} stock={stock} removeStock={this.props.removeStock} />)) : null
     return (
       <div>
         <h2>My Portfolio</h2>
           {
-            //render your portfolio stocks here
+            userStocksArr
           }
       </div>
     );
