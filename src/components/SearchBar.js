ec13b95bb51bbf7e0ddd6247ece60f54c16cc580
Andy Choi <wooseokchoi96@gmail.com>
2019-10-15 17:54:17 -0400

    Done.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,32 +1,35 @@
 import React from 'react';
 
-const SearchBar = () => {
+const SearchBar = (props) => {
+
   return (
-    <div>
+      <div>
 
-      <strong>Sort by:</strong>
-      <label>
-        <input type="radio" value="Alphabetically" checked={null} onChange={null}/>
-        Alphabetically
-      </label>
-      <label>
-        <input type="radio" value="Price" checked={null} onChange={null}/>
-        Price
-      </label>
-      <br/>
+        <strong>Sort by: </strong>
+        <label>
+          <input type="radio" name='sort' value="Alphabetically" checked={null} onChange={(e) => props.sortStock(e.target)}/>
+          Alphabetically
+        </label>
+        <label>
+          <input type="radio" name='sort' value="Price" checked={null} onChange={(e) => props.sortStock(e.target)}/>
+          Price
+        </label>
+        <br/>
 
-      <label>
-        <strong>Filter:</strong>
-        <select onChange={null}>
-          <option value="Tech">Tech</option>
-          <option value="Sportswear">Sportswear</option>
-          <option value="Finance">Finance</option>
-        </select>
-      </label>
+        <label>
+          <strong>Filter: </strong>
+          <select onChange={(e) => props.filterStock(e.target.value)}>
+            <option value="All">All</option>
+            <option value="Tech">Tech</option>
+            <option value="Sportswear">Sportswear</option>
+            <option value="Finance">Finance</option>
+          </select>
+        </label>
 
 
-    </div>
+      </div>
   );
+
 }
 
 
--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -5,20 +5,73 @@ import SearchBar from '../components/SearchBar'
 
 class MainContainer extends Component {
 
+  state = {
+    stocks : [],
+    showStocks : [],
+    portStocks : []
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:3000/stocks')
+    .then(resp => resp.json())
+    .then(data => {
+      this.setState({
+        stocks : data,
+        showStocks : data
+      })
+    })
+  }
+
+  addToPort = (stock) => {
+    let newPortStocks ;
+    if (!this.state.portStocks.includes(stock)) {
+      newPortStocks = [...this.state.portStocks, stock] ;
+      this.setState({ portStocks : newPortStocks})
+    }
+  }
+
+  removeFromPort = (stock) => {
+    this.setState({ portStocks : [...this.state.portStocks].filter(stockObj => stockObj !== stock)})
+  }
+
+  sortStock = (target) => {
+    let sorted;
+    switch(target.value){
+      case 'Alphabetically' :
+        sorted = this.state.showStocks.sort((a, b) => a.name > b.name ?  1 : -1 ) ;
+        break ;
+      case 'Price' :
+        sorted = this.state.showStocks.sort((a, b) => a.price > b.price ?  1 : -1 ) ;
+        break ;
+      default :
+        sorted = this.state.showStocks
+    } 
+    this.setState({ showStocks : sorted })
+  }
+
+  filterStock = (filterBy) => {
+    if (filterBy === 'All') {
+      this.setState({ showStocks : this.state.stocks })
+    } else {
+      let filtered = this.state.stocks.filter(stock => stock.type === filterBy) ;
+      this.setState({ showStocks : filtered })
+    }
+  }
+
   render() {
     return (
       <div>
-        <SearchBar/>
+        <SearchBar sortStock={this.sortStock} filterStock={this.filterStock}/>
 
           <div className="row">
             <div className="col-8">
 
-              <StockContainer/>
+              <StockContainer stocks={this.state.showStocks} addToPort={this.addToPort}/>
 
             </div>
             <div className="col-4">
 
-              <PortfolioContainer/>
+              <PortfolioContainer stocks={this.state.portStocks} removeFromPort={this.removeFromPort}/>
 
             </div>
           </div>
